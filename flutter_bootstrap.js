32bd915b1bdeff71faf31be6fc07082e720c6aae
Karthick P <pkarthick@qmaxsys.com>
2026-02-26 16:14:37 +0530

    updates: * Added dark mode * Added remaining fields: location value, dog size, motion sensitivity, auto-open related states, and time to remain open * Tool now shows IMP size and actuator details fetched from Google Sheets * Added an option to copy multiple logs from the log window * Fixed log scroll controller, placing the mouse inside the log window stops auto-scroll so the user can   review logs, auto-scroll resumes when the mouse leaves the window * Clicking on the agent URL opens a new window with GetState details

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "697590112"
+    serviceWorkerVersion: "2164573301"
   }
 });
